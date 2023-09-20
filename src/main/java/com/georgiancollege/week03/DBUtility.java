1863e5acfdf9ecbd3b12dc25f29744009ab6d93b
Priyansh Thakar <priyanshthakar@gmail.com>
2023-09-19 21:03:03 -0400

    Added dbutility

---
+++ src/main/java/com/georgiancollege/week03/DBUtility.java
@@ -0,0 +1,8 @@
+package com.georgiancollege.week03;
+
+public class DBUtility {
+    // variables for username, password and connection url
+    private static String user = "";
+    private static String pass = "";
+    private static String connectURL = "jdbc:mysql://172.31.22.43:3306/" + user;
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.georgiancollege.week03 {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
 
     opens com.georgiancollege.week03 to javafx.fxml;
     exports com.georgiancollege.week03;
